51f80254728341b6ca9089a4b9f2a3684bfca775
Jerome Horton <jeromehorton1@gmail.com>
2021-10-19 03:41:08 -0500

    worked on stretch goals' cd '

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,24 +1,3 @@
-// ### Required Features
-// Update the `cart.js` file to do the following:
-
-// - Create a global variable named `basket` and set it to an empty array.
-
-// - Create a function called `addItem`. It should:
-//  - take an input parameter for a string `item`
-//  - add the new item to the global array `basket`.
-//  - return `true` indicating the item was added
-
-// - Create a function called `listItems`. It should:
-//  - loop over the items in the `basket` array
-//  - console.log each individual item on a new line
-
-// - Create a function called `empty`. It should:
-//  - reset the `basket` to an empty array
-console.log('***** Cart Functions *****');
-// Make sure to test all functions here in the JS file!
-// We want to see how you are testing your code!!!
-
-
 let basket = []
 const maxItems = 5;
 
@@ -53,7 +32,6 @@ function isFull(){
   return false;
 }
 }
-
 
 // Items added into the array.
 addItem('Pineapple');
