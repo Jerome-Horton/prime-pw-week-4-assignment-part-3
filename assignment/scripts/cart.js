9b37cd25e60beb4bd84eb03623fa0653e4778396
Jerome Horton <jeromehorton1@gmail.com>
2021-10-18 21:57:21 -0500

    completed wk4 part 3 assignment

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,5 +1,3 @@
-
-
 // ### Required Features
 // Update the `cart.js` file to do the following:
 
@@ -22,34 +20,38 @@ console.log('***** Cart Functions *****');
 
 
 let basket = []
-let itemAdded = true;
-
 
 function addItem( item ){
- console.log( 'in addItem:', item );
- addItem.push( item );
- return basket;
+ console.log( 'in addItem:');
+// so push item into the array [basket]
+ basket.push( item );
+ console.log(basket);
+ return true;
+}
+// Verify if the items are in the basket, return true if it is.
+    function listItems(){
+    for (let i=0; i<basket.length; i++){
+     console.log('listItems:', basket[i]);
+  }
+    return basket;
+}
+// Create an empty array & reset it to an empty array.
+  function empty(){
+    console.log( 'in empty');
+    let basket = []; // assigning an empty array.
+    return basket;  // returning the array to verify it's empty.
 }
 
-function listItems( basket ){
-  console.log('in listItems:', basket );
-  for ( let i=0; i<basket.length; i++ )
-  
-}
 
-function empty( basket ){
-  console.log( 'in empty', basket);
-  return basket[basket.length-1] || null;
-}
-
-
-addItem('Juice');
-addItem( 'Mango');
-addItem( 'Avocado');
-addItem( 'Pineapple');
+// Items added into the array.
+addItem('Pineapple');
+addItem('Mango');
+addItem('Orange');
+// Show me what's in the array.
+console.log(addItem(basket));
 
 console.log(`Basket is ${basket}`);
 console.log('Adding apples (expect true)', addItem('apples'));
 console.log(`Basket is now ${basket}`);
-console.log( 'in Last Item Array:', empty(basket))
-console.log( 'in Last Item Array:', empty([]))
+console.log('listItems:', listItems());
+console.log('in empty basket:', empty());
